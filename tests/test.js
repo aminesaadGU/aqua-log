672ce3634525baec127b0c2768953a5e3e0dd967
aminesaadGU <64795389+aminesaadGU@users.noreply.github.com>
2026-05-10 15:02:20 +0100

    Add files via upload

--- tests/test.js
+++ tests/test.js
@@ -1,1 +1,9 @@
+const logger = require('../src/aquariumLogger');
 
+console.log('--- Aquarium Log Test ---');
+console.log('Adding test entry...');
+logger.addEntry({ tempC: 25.5, ph: 7.8, notes: 'Test entry' });
+console.log('Current entries:');
+console.log(logger.listEntries());
+console.log('CSV export:');
+console.log(logger.exportCSV());
